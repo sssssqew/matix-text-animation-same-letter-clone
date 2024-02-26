fde063f0074029fa4dd91614689915dd2d2fd95a
sssssqew <sunrise.in.my.dream@gmail.com>
2024-02-26 22:51:45 +0900

    매트릭스 애니메이션 구현하기

---
+++ app.js
@@ -0,0 +1,41 @@
+const paragraph = document.querySelector('pre')
+const text = paragraph.innerText
+const scrollbarWidth = 20
+const widthOfBrowser = document.documentElement.clientWidth - scrollbarWidth
+let isStarted = false
+
+function pickRandomNumber(n){
+  return Math.floor(Math.random() * n)
+}
+
+function pickRandomLetter(text){
+  const randomIndex = pickRandomNumber(text.length)
+  return text[randomIndex]
+}
+function createLetter(text){
+  console.log('creating letter...')
+  const span = document.createElement('span')
+  span.className = 'letter'
+  span.style.left = pickRandomNumber(widthOfBrowser) + 'px'
+  span.style.fontSize = pickRandomNumber(30) + 'px'
+  span.innerText = pickRandomLetter(text)
+  return span 
+}
+
+function startTextAnimation(){
+  if(!isStarted){
+    paragraph.innerText = ''
+    setInterval(function(){
+      const letter = createLetter(text)
+      document.body.appendChild(letter)
+      setInterval(function(){
+        letter.style.top = letter.offsetTop + 1 + 'px'
+      }, 10)
+    }, 100)
+
+    isStarted = true 
+  }
+}
+
+window.addEventListener('wheel', startTextAnimation)
+
