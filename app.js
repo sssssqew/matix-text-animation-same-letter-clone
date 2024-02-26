b7d46da4ce21793868407aad8e07bf79f701b5cf
sssssqew <sunrise.in.my.dream@gmail.com>
2024-02-27 07:02:42 +0900

    성능향상

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function createLetter(text, left, top){ // 문자열, 위치(x, y) 좌표를 입
   span.className = 'letter'
   span.style.left = left + 'px'
   span.style.fontSize = pickRandomNumber(maxFontSize) + 'px' // 최대 폰트크기보다 작은 글자 생성
-  span.innerText = text 
+  span.innerText = pickRandomLetter(text) 
   if(top) span.style.top = top + 'px' 
   return span 
 }
@@ -28,12 +28,12 @@ function createLetter(text, left, top){ // 문자열, 위치(x, y) 좌표를 입
 function displayLetter(){ // 랜덤글자를 생성하고 화면에 보여주기 위한 타이머 설정
   if(timer) clearTimeout(timer)
 
-  const randomLetter = pickRandomLetter(text) 
-  const letter = createLetter(randomLetter, pickRandomNumber(widthOfBrowser)) // 랜덤한 위치에 랜덤한 글자 생성 (브라우저 너비를 벗어나지 않는 범위에서)
+  // const randomLetter = pickRandomLetter(text) 
+  const letter = createLetter(text , pickRandomNumber(widthOfBrowser)) // 랜덤한 위치에 랜덤한 글자 생성 (브라우저 너비를 벗어나지 않는 범위에서)
   document.body.appendChild(letter) // 화면에 랜덤글자 디스플레이 
   setInterval(function(){ // 생성한 랜덤글자를 Y축으로 이동시키기 위한 타이머 설정 
     letter.style.top = letter.offsetTop + 7 + 'px' // 생성한 랜덤글자를 아래방향으로 이동
-    const cloneLetter = createLetter(randomLetter, letter.offsetLeft, letter.offsetTop) // 처음에 생성한 랜덤글자와 동일한 글자를 현재 Y측 위치에 복제
+    const cloneLetter = createLetter(text, letter.offsetLeft, letter.offsetTop) // 처음에 생성한 랜덤글자와 동일한 글자를 현재 Y측 위치에 복제
     document.body.appendChild(cloneLetter) // 복제된 랜덤글자를 화면에 디스플레이 
   }, 30)
   timer = setTimeout(displayLetter, 100)
